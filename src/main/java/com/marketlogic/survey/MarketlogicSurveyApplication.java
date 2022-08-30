58383cdae261eeba84cb8271420000d83fac2518
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-30 14:31:37 -0300

    Initial commit

---
+++ src/main/java/com/marketlogic/survey/MarketlogicSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.marketlogic.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketlogicSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketlogicSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketlogic/survey/MarketlogicSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketlogic.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketlogicSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
